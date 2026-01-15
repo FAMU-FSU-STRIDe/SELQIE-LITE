0e8c6b7e35ad41e6e8568b45ecffee837f1ac96c
rkacz17 <rykaczmarczyk@gmail.com>
2026-01-15 18:43:08 -0500

    Switch latch servo control to Jetson.GPIO

---
+++ latch/launch/latch.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="latch",
+                executable="latch_node",
+                name="latch_node",
+                output="screen",
+                parameters=[
+                    {
+                        "gpio_pin": 33,
+                        "gpio_mode": "BOARD",
+                        "period_us": 20000,
+                        "min_pulse_us": 1000,
+                        "max_pulse_us": 2000,
+                        "min_angle_deg": 0.0,
+                        "max_angle_deg": 180.0,
+                        "neutral_angle_deg": 90.0,
+                        "auto_enable": True,
+                        "startup_angle_deg": 90.0,
+                    }
+                ],
+            )
+        ]
+    )
